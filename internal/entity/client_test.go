1e0098bb1ae3e90cc01dd7fce6f6002f538663ae
Thaigo Bianeck <thgbianeck@gmail.com>
2024-07-23 22:07:32 -0300

    Alterando Clientes

---
+++ internal/entity/client_test.go
@@ -0,0 +1,41 @@
+package entity_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/thgbianeck/bnck-ms-wallet/internal/entity"
+	"testing"
+)
+
+func TestCreateNewClient(t *testing.T) {
+	client, err := entity.NewClient("John Doe", "johndoe@mail.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "John Doe", client.Name)
+	assert.Equal(t, "johndoe@mail.com", client.Email)
+}
+
+func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := entity.NewClient("", "")
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := entity.NewClient("John Doe", "johndoe@mail.com")
+
+	err := client.Update("Jane Doe", "janedoe@mail.com")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "Jane Doe", client.Name)
+	assert.Equal(t, "janedoe@mail.com", client.Email)
+	assert.True(t, client.UpdatedAt.After(client.CreatedAt))
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := entity.NewClient("John Doe", "johndoe@mail.com")
+	err := client.Update("", "janedoe@mail.com")
+
+	assert.NotNil(t, err)
+	assert.Error(t, err, "name is required")
+}
