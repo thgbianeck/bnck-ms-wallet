f8ea85cab63908618f4ab4b76d2421cd920ed9bd
Thaigo Bianeck <thgbianeck@gmail.com>
2024-07-24 17:30:35 -0300

    Implementando e Testando Transactions

--- internal/database/client_db_test.go
+++ internal/database/client_db_test.go
@@ -1,1 +1,50 @@
 package database_test
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/suite"
+	"github.com/thgbianeck/bnck-ms-wallet/internal/database"
+	"github.com/thgbianeck/bnck-ms-wallet/internal/entity"
+	"testing"
+)
+
+type ClientDBTestSuit struct {
+	suite.Suite
+	db       *sql.DB
+	clientDB *database.ClientDB
+}
+
+func (s *ClientDBTestSuit) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+	db.Exec("CREATE TABLE clients (id varchar(255), name varchar(255), email varchar(255), created_at date, updated_at date)")
+	s.clientDB = database.NewClientDB(db)
+}
+
+func (s *ClientDBTestSuit) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+}
+
+func TestClientDBTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientDBTestSuit))
+}
+
+func (s *ClientDBTestSuit) TestSave() {
+	client, _ := entity.NewClient("John Doe", "johndoe@mail.com")
+	err := s.clientDB.Save(client)
+	s.Nil(err)
+}
+
+func (s *ClientDBTestSuit) TestGet() {
+	client, _ := entity.NewClient("John Doe", "johndoe@mail.com")
+	s.clientDB.Save(client)
+
+	clientDB, err := s.clientDB.Get(client.ID)
+	s.Nil(err)
+	s.Equal(client.ID, clientDB.ID)
+	s.Equal(client.Name, clientDB.Name)
+	s.Equal(client.Email, clientDB.Email)
+}
--- internal/database/transaction_db_test.go
+++ internal/database/transaction_db_test.go
@@ -1,1 +1,59 @@
 package database_test
+
+import (
+	"database/sql"
+	"github.com/stretchr/testify/suite"
+	"github.com/thgbianeck/bnck-ms-wallet/internal/database"
+	"github.com/thgbianeck/bnck-ms-wallet/internal/entity"
+	"testing"
+)
+
+type TransactionDBTestSuite struct {
+	suite.Suite
+	db            *sql.DB
+	client        *entity.Client
+	client2       *entity.Client
+	accountFrom   *entity.Account
+	accountTo     *entity.Account
+	transactionDB *database.TransactionDB
+}
+
+func (s *TransactionDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+	db.Exec("CREATE TABLE accounts (id varchar(255), client_id varchar(255), balance double, created_at date, updated_at date)")
+	db.Exec("CREATE TABLE clients (id varchar(255), name varchar(255), email varchar(255), created_at date, updated_at date)")
+	db.Exec("CREATE TABLE transactions (id varchar(255), account_id_from varchar(255), account_id_to varchar(255), amount double, created_at date, updated_at date)")
+	client, err := entity.NewClient("richard", "rich@email.com")
+	s.Nil(err)
+	s.client = client
+	client2, err := entity.NewClient("richard2", "rich2@email.com")
+	s.Nil(err)
+	s.client2 = client2
+	accountFrom := entity.NewAccount(s.client)
+	accountFrom.Balance = 1000
+	s.accountFrom = accountFrom
+	accountTo := entity.NewAccount(s.client2)
+	accountTo.Balance = 1000
+	s.accountTo = accountTo
+	s.transactionDB = database.NewTransactionDB(db)
+}
+
+func (s *TransactionDBTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+	s.db.Exec("DROP TABLE accounts")
+	s.db.Exec("DROP TABLE transactions")
+}
+
+func TestTransactionDBTestSuite(t *testing.T) {
+	suite.Run(t, new(TransactionDBTestSuite))
+}
+
+func (s *TransactionDBTestSuite) TesteCreate() {
+	transaction, err := entity.NewTransaction(s.accountFrom, s.accountTo, 100)
+	s.Nil(err)
+	err = s.transactionDB.Create(transaction)
+	s.Nil(err)
+}
